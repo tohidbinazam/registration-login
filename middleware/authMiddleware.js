1b367cb1ce30ab857aac3fb9a9b3fbbea6ce52f1
tohidbinazam <tohidbinazamsunny1@gmail.com>
2022-06-14 13:43:54 +0600

    Done

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,16 +5,20 @@ const adminModel = require('../models/adminModel');
 // Auth Middleware
 const authCheck = async (req, res, next) => {
     
-    const token = req.headers.authorization.split(' ')[1]
-    const verify = jwt.verify(token, process.env.JWT_SECRET)
 
-    if (token && verify) {
-        req.user = await adminModel.findById(verify.id)
-        console.log('ok');
-        next()
+    if (req.headers.authorization) {
+        const token = req.headers.authorization.split(' ')[1]
+        try{
+            const verify = jwt.verify(token, process.env.JWT_SECRET)
+            req.user = await adminModel.findById(verify.id)
+            next()
+        }catch(error){
+            res.status(200).json(error.message)
+        }
     } else {
-        console.log('wrong');
+        res.status(200).json('Token missing')
     }
+
 }
 
 module.exports = { authCheck }
